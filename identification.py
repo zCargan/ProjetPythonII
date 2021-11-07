cd618943c65d308450aa442fb282031685d09b42
zCargan <64102236+zCargan@users.noreply.github.com>
2021-11-07 21:36:10 +0100

    modification de l'identification
    ajout d'une liste des privilèges

--- identification.py
+++ identification.py
@@ -2,6 +2,8 @@ import csv
 import os
 
 dic_password = {}
+all_titles = []
+all_privileges = []  # contains all the privileges allowed for the discord
 
 
 # def search_files(folder):
@@ -19,7 +21,8 @@ def complete_password(dic):
             for line in file_password:
                 key = line[0]
                 if key == "ï»¿user":
-                    pass
+                    for i in line:
+                        all_titles.append(i)
                 else:
                     if key not in dic:
                         dic[key] = []
@@ -39,13 +42,28 @@ def identification(dic):
         if dic[user][0] == password:
             if dic[user][1] == "X":
                 print("Successful authentication, u are admin too")
+                print("Musique : Allowed \nLogiciel : Allowed \nMeteo : Allowed \ngestion_acces : Allowed")
             else:
                 print("Successful authentication, u are a simple user")
+                print("Musique : Allowed \nLogiciel : Allowed \nMeteo : Allowed \ngestion_acces : Denied")
         else:
             print("incorrect password")
     else:
-        print("user not found")
+        print("user not found, do u want you register?")
+        new_user = input("What is your username?")
+        dic_password[new_user] = []
+        password_user = input("Password?")
+        dic_password[new_user].append(password_user)
+
+
+def define_privileges(list):
+    for i in range(2, len(list)):
+        # print(list[i]) // récupère les privilège
+        all_privileges.append(all_titles[i])
 
 
 complete_password(dic_password)
 identification(dic_password)
+define_privileges(all_titles)
+print(dic_password)
+
