8d3758ae51cb1031722cb895c202f0b0b7124dbf
zCargan <64102236+zCargan@users.noreply.github.com>
2021-11-09 23:19:42 +0100

    v3

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from connected import connected
-from create_accout_script import CreateUserFunction
+from connected import connected_user
+# from create_accout_script import CreateUserFunction
+from create_accout_window import ShowCreatingTool
 from identification import dic_password, identification_user, complete_password
+from mongo_connection import data_mongo
 
 
 class Identification(BoxLayout):
@@ -26,7 +28,8 @@ class Identification(BoxLayout):
         self.add_widget(self.identification_message)
 
     def input_username_function(self):
-        self.input_username = TextInput(text="username", font_size=20, pos_hint={'x': .38, 'y': 0}, size_hint=(.24, .005), )
+        self.input_username = TextInput(text="username", font_size=20, pos_hint={'x': .38, 'y': 0},
+                                        size_hint=(.24, .005))
         self.add_widget(self.input_username)
 
     # def labelPassword(self):
@@ -35,7 +38,8 @@ class Identification(BoxLayout):
     #     self.add_widget(self.password)
 
     def input_password_function(self):
-        self.input_password = TextInput(text="Password", font_size=20, pos_hint={'x': .38, 'y': 0}, size_hint=(.24, .005))
+        self.input_password = TextInput(text="Password", font_size=20, pos_hint={'x': .38, 'y': 0},
+                                        size_hint=(.24, .005))
         self.add_widget(self.input_password)
 
     def button_identification(self):
@@ -60,18 +64,26 @@ class Identification(BoxLayout):
         self.add_widget(self.create_account_button)
 
     def sign_in(self, instance):
+        """
+        check is the username and the password are corrects, if is, show the message "connected"
+        :param instance:
+        :return: the type of connection (yes or not)
+        """
         user = self.input_username.text
         password = self.input_password.text
-        identification_user(dic_password, user, password)
-        self.identification_message.text = connected(dic_password, user, password)
+        self.identification_message.text = connected_user(identification_user(data_mongo(), user, password))
         self.identification_message.color = [0, 1, 0, 1]
-
 
     def password_forget(self):
         pass
 
-    def create_account(self):
-        CreateUserFunction()
+    def create_account(self, instance):
+        """
+        run the page of creation's account
+        :param instance:
+        :return: the user's datas to the database
+        """
+        ShowCreatingTool().run()
 
 
 class CheckUser(App):
@@ -82,7 +94,6 @@ class CheckUser(App):
 
 
 if __name__ == '__main__':
-    complete_password(dic_password)
     CheckUser().run()
 
     # if user not in dic_password:
